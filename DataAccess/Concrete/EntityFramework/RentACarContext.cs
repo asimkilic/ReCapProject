9fd8c4734741401a91fb9ed7147753805520bc4d
asimkilic <a.asim.kilic@gmail.com>
2021-03-07 15:07:41 +0300

    8.gün son

--- DataAccess/Concrete/EntityFramework/RentACarContext.cs
+++ DataAccess/Concrete/EntityFramework/RentACarContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,6 +8,12 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class RentACarContext:DbContext
     {
-
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb; Database=ReCapDb; Trusted_connection=true;");
+        }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Colors { get; set; }
     }
 }
